1deec77bef30e28f5286b4eaea56ef254e90d831
Manuel Morales <luis_issc@hotmail.com>
2020-05-26 20:27:26 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,38 @@
+// ConsoleApplication2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+bool palindromo(string a) {
+	int j = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		j = a.size() - 1 - i;
+		if (a[i] != a[j]) {
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+    string a = "anitalavalatinaa";
+	if (palindromo(a)) {
+		cout << "Es un palindromo";
+	}
+	else
+		cout << "pues no lo Es un palindromo";
+	return 1;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
